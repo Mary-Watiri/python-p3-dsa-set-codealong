4779f6a426b1a5b926df4a329b464489b296b142
Mary Watiri <watirimary30@gmail.com>
2024-03-25 22:53:16 +0300

    Completed Assignment

--- lib/MySet.py
+++ lib/MySet.py
@@ -1,3 +1,28 @@
 class MySet:
+    def __init__(self, enumerable=None):
+        self.dictionary = {}
+        if enumerable is not None:
+            for value in enumerable:
+                self.add(value)
 
-    pass
+    def has(self, value):
+        return value in self.dictionary
+
+    def add(self, value):
+        self.dictionary[value] = True
+        return self
+
+    def delete(self, value):
+        if self.has(value):
+            del self.dictionary[value]
+        return self
+
+    def size(self):
+        return len(self.dictionary)
+
+    def clear(self):
+        self.dictionary.clear()
+
+    def __str__(self):
+        return f"MySet: {{{', '.join(str(key) for key in self.dictionary.keys())}}}"
+
